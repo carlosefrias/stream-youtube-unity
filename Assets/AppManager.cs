ed9d882a3b2b4aabd84db8c0ea5d48134966cbb1
Carlos Frias <carlosefrias@gmail.com>
2019-10-16 09:21:04 +0100

    Streaming youtube videos on unity.

---
+++ Assets/AppManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Linq;
+using YoutubeExtractor;
+public class AppManager : MonoBehaviour
+{
+    private const string UrlVideo = "https://www.youtube.com/watch?v=PTSDBLr62qI&t=65s";
+    private const string UrlPdf = "https://steve-parker.org/sh/cheatsheet.pdf";
+    
+    private const int Quality = 1080;
+    public MediaPlayerCtrl mediaPlayerCtrl;
+
+    private void Start()
+    {
+        Application.OpenURL(UrlPdf);
+        //Run();
+    }
+    
+    public async void Run()
+    {
+        var videoInfos = await DownloadUrlResolver.GetDownloadUrlsAsync(UrlVideo);
+        var video = videoInfos.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == Quality);  
+        
+        print($"video null? {video}");
+        
+        if(video.RequiresDecryption)
+        {
+            DownloadUrlResolver.DecryptDownloadUrl(video);
+        }
+        mediaPlayerCtrl.m_strFileName = video.DownloadUrl;
+        
+        print($"mediaPlayerCtrl.m_strFileName: {mediaPlayerCtrl.m_strFileName}");
+    }
+}
